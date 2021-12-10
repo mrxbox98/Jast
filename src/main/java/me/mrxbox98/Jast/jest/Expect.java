45f085b0ae5e2a7dc3014eec05f5476dd73d6e80
Mrxbox98 <29240392+mrxbox98@users.noreply.github.com>
2021-12-10 10:13:49 -0800

    Jest Compatible Syntax WIP

--- src/main/java/me/mrxbox98/Jast/jest/Expect.java
+++ src/main/java/me/mrxbox98/Jast/jest/Expect.java
@@ -2,42 +2,80 @@ package me.mrxbox98.Jast.jest;
 
 public class Expect {
 
+    /**
+     * The actual value.
+     */
     Class<?> actual;
 
+    /**
+     * The opposite of the actual value.
+     */
     Expect not = not();
 
+    /**
+     * Whether to flip.
+     */
     boolean notCheck = false;
 
+    /**
+     * Creates a new Expect instance.
+     * @param actual The actual value.
+     */
     public Expect(Class<?> actual) {
         this.actual = actual;
     }
 
+    /**
+     * Flips the expect instance.
+     */
     public Expect not()
     {
         notCheck = true;
         return this;
     }
 
+    /**
+     * Checks if the actual value is equal to the expected value.
+     * @param expected The expected value.
+     * @return true if the actual value is equal to the expected value. Otherwise, false.
+     */
     public boolean toBe(Class<?> expected)
     {
         return notCheck != (actual == expected);
     }
 
+    /**
+     * Checks if the actual value is equal to the expected value.
+     * @param expected The expected value.
+     * @return true if the actual value is equal to the expected value. Otherwise, false.
+     */
     public boolean toEqual(Class<?> expected)
     {
         return notCheck != (actual.equals(expected));
     }
 
+    /**
+     * Checks if the actual value is null.
+     * @return true if the actual value is null. Otherwise, false.
+     */
     public boolean toBeNull()
     {
         return notCheck != (actual == null);
     }
 
+    /**
+     * Checks if the actual value is not null.
+     * @return true if the actual value is not null. Otherwise, false.
+     */
     public boolean toBeDefined()
     {
         return notCheck == (actual == null);
     }
 
+    /**
+     * Checks if the actual value is null.
+     * @return true if the actual value is null. Otherwise, false.
+     */
     public boolean toBeUndefined()
     {
         return notCheck != (actual == null);
@@ -53,6 +91,11 @@ public class Expect {
         return notCheck != (actual == null);
     }
 
+    /**
+     * Checks if the actual value is greater than the supplied value.
+     * @param value The value to compare to.
+     * @return true if the actual value is greater than the supplied value. Otherwise, false.
+     */
     public boolean toBeGreaterThan(double value)
     {
         try
@@ -137,11 +180,37 @@ public class Expect {
         }
     }
 
+    public boolean toBeLessThanOrEqual(int value)
+    {
+        try
+        {
+            return notCheck != (Integer.parseInt(String.valueOf(actual))<=value);
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+    }
+
     public boolean toBeCloseTo(double value)
     {
         try
         {
-            return notCheck != (Double.parseDouble(String.valueOf(actual))<=value);
+
+
+            return notCheck != (Math.abs(Double.parseDouble(String.valueOf(actual))-value)<Double.MIN_NORMAL);
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+    }
+
+    public boolean toBeCloseTo(int value)
+    {
+        try
+        {
+            return notCheck != (Integer.parseInt(String.valueOf(actual))==value);
         }
         catch (NumberFormatException e)
         {
